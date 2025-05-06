30352ba0a7d8c5e8aed5f8e9f2eb8e9710cdb4b6
Dave Cheng <firstdreamlord@gmail.com>
2025-05-06 12:43:18 +0000

    increase by 50% the left side

--- src/components/ui/sidebar.tsx
+++ src/components/ui/sidebar.tsx
@@ -1,3 +1,4 @@
+
 "use client"
 
 import * as React from "react"
@@ -21,9 +22,9 @@ import {
 
 const SIDEBAR_COOKIE_NAME = "sidebar_state"
 const SIDEBAR_COOKIE_MAX_AGE = 60 * 60 * 24 * 7
-const SIDEBAR_WIDTH = "16rem"
-const SIDEBAR_WIDTH_MOBILE = "18rem"
-const SIDEBAR_WIDTH_ICON = "3rem"
+const SIDEBAR_WIDTH = "24rem" // Increased from 16rem
+const SIDEBAR_WIDTH_MOBILE = "27rem" // Increased from 18rem
+const SIDEBAR_WIDTH_ICON = "4.5rem" // Increased from 3rem
 const SIDEBAR_KEYBOARD_SHORTCUT = "b"
 
 type SidebarContext = {
@@ -761,3 +762,5 @@ export {
   SidebarTrigger,
   useSidebar,
 }
+
+    
